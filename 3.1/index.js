36f3ed25cf67084d711e5bbda552e0a77563a829
Bobovino <rodrigomartineztabernero97@gmail.com>
2023-09-16 13:08:10 +0200

    3.5

--- 3.1/index.js
+++ 3.1/index.js
@@ -6,7 +6,9 @@ const app = express()
 
 const dataFilePath = path.join(__dirname, './persons.json')
 
-// Read data from the file
+app.use(express.json());   
+
+// Read/Write data from the file
 const readData = () => {
     try {
       const rawData = fs.readFileSync(dataFilePath, 'utf8')
@@ -26,7 +28,43 @@ const readData = () => {
     }
 }
 
+//Create--------------------------------------------
 
+const generateId = () => {
+    data=readData()
+    const maxId = data.length > 0
+      ? Math.max(...data.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/persons', (request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+    const body = request.body
+    console.log('Received body:', request.body);
+    let data= readData()
+
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    }
+  
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number,
+      important: body.important || false,
+      
+    }
+  
+    data = data.concat(person)
+    saveData(data)
+    response.json(person)
+  })
+
+
+//READ--------------------------------------------
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
   })
@@ -53,6 +91,8 @@ app.get('/api/persons', (request, response) => {
         response.status(404).send({ error: 'Person not found' });
     }
 })
+
+//DELETE--------------------------------------------
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
